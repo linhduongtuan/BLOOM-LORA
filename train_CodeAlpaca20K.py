6249d01c832d9f9abd5b4ecfb9f193dd5aaf32a7
Linh T. Duong <linhduongtuan@yahoo.com>
2023-03-27 21:02:36 +0700

    Add files via upload

---
+++ train_CodeAlpaca20K.py
@@ -0,0 +1,226 @@
+import os
+import sys
+
+import torch
+print(torch.__version__)
+import torch.nn as nn
+#import bitsandbytes as bnb
+from datasets import load_dataset
+import transformers
+from transformers import Trainer, TrainingArguments
+from transformers import BloomForCausalLM, BloomTokenizerFast
+
+from peft import (
+    prepare_model_for_int8_training,
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+)
+
+# from huggingface_hub import login, HfFolder
+
+# login(
+#   token="", # ADD YOUR TOKEN HERE
+#   add_to_git_credential=True
+# )
+
+
+# optimized for RTX 4090. for larger GPUs, increase some of these?
+MICRO_BATCH_SIZE = 4  # this could actually be 5 but i like powers of 2
+BATCH_SIZE = 128
+GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
+EPOCHS = 3  # we don't always need 3 tbh
+LEARNING_RATE = 3e-4  # the Karpathy constant
+CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
+LORA_R = 8
+LORA_ALPHA = 16
+LORA_DROPOUT = 0.05
+VAL_SET_SIZE = 2000
+DATA_PATH = "code_alpaca_20k.json"
+OUTPUT_DIR = "BLOOM-alpaca-7b1-c"
+
+## Choose a model to finetune 
+# model_name = 'bigscience/bloom-560m'
+#model_name = 'bigscience/bloom-1b1'
+#model_name = 'bigscience/bloom-3b'
+model_name = 'bigscience/bloom-7b1'
+#model_name = 'bigscience/bloom' # for 176B parameters
+
+repository_id = "bloom-" + model_name
+
+device_map = "auto"
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+ddp = world_size != 1
+if ddp:
+    device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+    GRADIENT_ACCUMULATION_STEPS = GRADIENT_ACCUMULATION_STEPS // world_size
+
+model = BloomForCausalLM.from_pretrained( 
+    model_name,
+    device_map='auto',
+    load_in_8bit=True,
+)
+tokenizer = BloomTokenizerFast.from_pretrained('bigscience/bloom')
+#tokenizer = AutoTokenizer.from_pretrained('bigscience/bloom')
+config = LoraConfig(
+    r=LORA_R,
+    lora_alpha=LORA_ALPHA,
+    #target_modules=TARGET_MODULES,
+    lora_dropout=LORA_DROPOUT,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+model = get_peft_model(model, config)
+tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+data = load_dataset("json", data_files=DATA_PATH)
+
+
+def generate_prompt(data_point):
+    # sorry about the formatting disaster gotta move fast
+    if data_point["input"]:
+        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Input:
+{data_point["input"]}
+
+### Response:
+{data_point["output"]}"""
+    else:
+        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Response:
+{data_point["output"]}"""
+
+
+def tokenize(prompt):
+    # there's probably a way to do this with the tokenizer settings
+    # but again, gotta move fast
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=CUTOFF_LEN + 1,
+        padding="max_length",
+    )
+    return {
+        "input_ids": result["input_ids"][:-1],
+        "attention_mask": result["attention_mask"][:-1],
+    }
+
+
+def generate_and_tokenize_prompt(data_point):
+    # This function masks out the labels for the input,
+    # so that our loss is computed only on the response.
+    user_prompt = (
+        (
+            f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Input:
+{data_point["input"]}
+
+### Response:
+"""
+        )
+        if data_point["input"]
+        else (
+            f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Response:
+"""
+        )
+    )
+    len_user_prompt_tokens = (
+        len(
+            tokenizer(
+                user_prompt,
+                truncation=True,
+                max_length=CUTOFF_LEN + 1,
+                padding="max_length",
+            )["input_ids"]
+        )
+        - 1
+    )  # no eos token
+    full_tokens = tokenizer(
+        user_prompt + data_point["output"],
+        truncation=True,
+        max_length=CUTOFF_LEN + 1,
+        padding="max_length",
+    )["input_ids"][:-1]
+    return {
+        "input_ids": full_tokens,
+        "labels": [-100] * len_user_prompt_tokens
+        + full_tokens[len_user_prompt_tokens:],
+        "attention_mask": [1] * (len(full_tokens)),
+    }
+
+
+if VAL_SET_SIZE > 0:
+    train_val = data["train"].train_test_split(
+        test_size=VAL_SET_SIZE, shuffle=True, seed=42
+    )
+    train_data = train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+    val_data = train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+else:
+    train_data = data['train'].shuffle().map(generate_and_tokenize_prompt)
+    val_data = None
+
+trainer = Trainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=val_data,
+    args=TrainingArguments(
+        per_device_train_batch_size=MICRO_BATCH_SIZE,
+        gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+        warmup_steps=100,
+        num_train_epochs=EPOCHS,
+        learning_rate=LEARNING_RATE,
+        fp16=True,
+        logging_steps=20,
+        evaluation_strategy="steps" if VAL_SET_SIZE > 0 else "no",
+        save_strategy="steps",
+        eval_steps=200 if VAL_SET_SIZE > 0 else None,
+        save_steps=200,
+        output_dir=OUTPUT_DIR, #output_dir=repository_id,
+        save_total_limit=3,
+        load_best_model_at_end=True if VAL_SET_SIZE > 0 else False,
+        ddp_find_unused_parameters=False if ddp else None,
+        # torch_compile=True, # optimizations
+        # optim="adamw_torch_fused", # improved optimizer
+        # push to hub parameters
+        report_to='wandb',
+        # push_to_hub=True,
+        # hub_strategy="every_save",
+        # hub_model_id=repository_id,
+        # hub_token=HfFolder.get_token(),
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+model.config.use_cache = False
+
+old_state_dict = model.state_dict
+model.state_dict = (
+    lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+).__get__(model, type(model))
+
+if torch.__version__ >= "2" and sys.platform != 'win32':
+    model = torch.compile(model)
+
+trainer.train(resume_from_checkpoint = False)
+
+model.save_pretrained(OUTPUT_DIR)
+
+# trainer.create_model_card()
+# trainer.push_to_hub()
+
+print("\n If there's a warning about missing keys above, please disregard :)")
